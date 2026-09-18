dcffe51e4f6d467887296bedafa61922b7ec02b2
AZAwhite13 <youremail@example.com>
2026-09-18 11:39:36 +0500

    first commit

---
+++ home.js
@@ -0,0 +1,122 @@
+// let harorat = 19;
+
+// if (harorat < 0) {
+//     console.log("Sovuq");
+// } else {
+//     console.log("Iliq");
+// }
+
+
+// let a = 3.3;
+// let b = 3.29;
+
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+// let son = 4;
+
+// if (son > 0) {
+//     console.log("Musbat");
+// } else if (son < 0) {
+//     console.log("Manfiy");
+// } else {
+//     console.log("Musbat ham manfiy ham emas");
+// }
+
+
+// let son = 5;
+
+// if (son % 2 == 0) {
+//     console.log("Juft");
+// } else {
+//     console.log("Toq");
+// }
+
+
+// let ball = 67;
+
+// if (ball >= 60) {
+//     console.log("O'tdi");
+// } else {
+//     console.log("Qayta topshiring");
+// }
+
+// let ball = 78;
+
+// if (ball > 90) {
+//     console.log("A'lo");
+// } else if (ball >= 70) {
+//     console.log("Yaxshi");
+// } else if (ball >= 50) {
+//     console.log("Qoniqarli");
+// } else {
+//     console.log("Yomon");
+// }
+
+
+// let oy = 2;
+
+// if (oy == 12 || oy == 1 || oy == 2) {
+//     console.log("Qish");
+// } else if (oy == 3 || oy == 4 || oy == 5) {
+//     console.log("Bahor");
+// } else if (oy == 6 || oy == 7 || oy == 8) {
+//     console.log("Yoz");
+// } else if (oy == 9 || oy == 10 || oy == 11) {
+//     console.log("Kuz");
+// } else {
+//     console.log("Noto'g'ri oy");
+// }
+
+
+// let yil = 2024;
+
+// if (yil % 4 == 0) {
+//     console.log("Kabisa yili");
+// } else {
+//     console.log("Kabisa yili emas");
+// }
+
+
+// let a = 80;
+// let b = 89;
+// let c = 71;
+
+// let max = a;
+
+// if (b > max) {
+//     max = b;
+// }
+
+// if (c > max) {
+//     max = c;
+// }
+
+// console.log(max);
+
+
+// let matematika = 74;
+// let fizika = 63;
+// let kimyo = 60;
+
+// if (matematika >= 60 && fizika >= 60 && kimyo >= 60) {
+//     console.log("Qabul qilish");
+// } else {
+//     console.log("Qabul qilinmaydi");
+// }
+
+
+// let son = 15;
+
+// if (son % 3 == 0 && son % 5 == 0) {
+//     console.log("FizzBuzz");
+// } else if (son % 3 == 0) {
+//     console.log("Fizz");
+// } else if (son % 5 == 0) {
+//     console.log("Buzz");
+// } else {
+//     console.log(son);
+// }
